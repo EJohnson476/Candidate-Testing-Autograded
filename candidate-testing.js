c59fce12bc046ce123c2c45504f734a946fc8d3c
E J <johnsonelijah440@gmail.com>
2023-09-17 11:08:09 -0500

    <testPart1>

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,15 +1,33 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
+/*
+questions.push();
+correctAnswers.push();
+questions.push();
+correctAnswers.push();
+questions.push();
+correctAnswers.push();
+questions.push();
+correctAnswers.push();
+questions.push();
+correctAnswers.push();
+*/
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = input.question("What is your name? ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = input.question("What is the answer to life, the universe, and everything? ");
+let correctAnswer = '42';
+let candidateAnswer = question;
 
-
+if (candidateAnswer === correctAnswer)
+{
+  console.log("Your answer was correct.");
+}
+else
+{
+  console.log("Your answer was incorrect.");
+}
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
@@ -18,15 +36,18 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  //candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+ /* for (i = 0; i <= 5; i++)
+  {
+    candidateAnswers.push(input.question(`${question[i]}`));
+  }
 
 }
-
+*/
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
