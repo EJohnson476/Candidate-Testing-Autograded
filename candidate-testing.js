4ccee22c5251914dc6f70eaa3298b340246336b7
E J <johnsonelijah440@gmail.com>
2023-09-17 21:57:51 -0500

    <candidateTestingFinal>

--- candidate-testing.js
+++ candidate-testing.js
@@ -22,8 +22,8 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space? " , 'True or false: 5 kilometer == 5000 meters? ' , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride" , "true" , "40" , "Trajectory" , "3"];
 let candidateAnswers;
 
 
@@ -35,31 +35,37 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-    candidateAnswer = input.question(question);
+   // candidateAnswer = input.question(question);
  
-  /* for (i = 0; i <= 5; i++)
+   for (i = 0; i <= 5; i++)
   {
     candidateAnswers.push(input.question(`${question[i]}`));
   }
-*/
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let rightAnswers = 0;
+  let wrongAnswers = 0;
 
-  if (candidateAnswer === correctAnswer)
+for (i = 0; i <= 4; i++)
 {
-  console.log("Your answer was correct.");
+  if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase())
+    {
+      console.log("Your answer was correct.");
+      rightAnswers++;
+    }
+  else
+    {
+      console.log(`Your answer was incorrect. The correct answer was ${correctAnswers[i]}.`);
+      wrongAnswers++;
+    }
 }
-else
-{
-  console.log("Your answer was incorrect.");
-}
-
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  grade = ((rightAnswers) / (questions.length) * 100);
 
   return grade;
 }
